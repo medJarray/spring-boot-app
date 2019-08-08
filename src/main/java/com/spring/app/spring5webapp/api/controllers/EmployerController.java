0c44afda5a6c06e5481f112f2bc092d4f1fdab8d
ABDERRAOUF MAKHLOUF <abderraouf.makhlouf@ext.adeo.com>
2019-08-08 09:59:22 +0200

    chore() extract empler list in csv file

--- src/main/java/com/spring/app/spring5webapp/api/controllers/EmployerController.java
+++ src/main/java/com/spring/app/spring5webapp/api/controllers/EmployerController.java
@@ -2,6 +2,7 @@ package com.spring.app.spring5webapp.api.controllers;
 
 import com.spring.app.spring5webapp.api.EmployerApi;
 import com.spring.app.spring5webapp.api.controllers.exception.EmployerNotFoundException;
+import com.spring.app.spring5webapp.common.CsvFile;
 import com.spring.app.spring5webapp.model.CreateEmployer;
 import com.spring.app.spring5webapp.model.EmployerElement;
 import com.spring.app.spring5webapp.services.EmployerService;
@@ -10,13 +11,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.List;
 
@@ -24,10 +30,9 @@ import java.util.List;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class EmployerController implements EmployerApi {
 
+    private static final String FILE_NAME = "employer-extraction";
     private final MessageSource messageSource;
-
     private final EmployerService employerService;
-
 
     @Override
     public ResponseEntity<EmployerElement> createEmployer(@ApiParam(value = "Ludo", required = true) @Valid @RequestBody CreateEmployer employerToCreate) {
@@ -39,6 +44,20 @@ public class EmployerController implements EmployerApi {
         List<EmployerElement> allEmployer = employerService.getAllEmployer();
         return new ResponseEntity(allEmployer, null, allEmployer.isEmpty() ? HttpStatus.NO_CONTENT : HttpStatus.OK);
 
+    }
+
+    @Override
+    public ResponseEntity<Resource> downloadCsvExtraction() throws InterruptedException, IOException {
+
+        List<EmployerElement> allEmployer = employerService.getAllEmployer();
+
+        // write csv
+        InputStreamResource resource = CsvFile.writeCsv(allEmployer);
+
+        return ResponseEntity.ok()
+                             .contentType(MediaType.parseMediaType("application/octet-stream"))
+                             .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + FILE_NAME + ".csv")
+                             .body(resource);
     }
 
     public ResponseEntity<EmployerElement> getEmployerByName(@RequestParam("name") String name) {
